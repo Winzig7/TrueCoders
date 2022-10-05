b700001ef796daa6df0d9eb50c155327e8408d6e
Winzig7 <114783331+Winzig7@users.noreply.github.com>
2022-10-04 23:41:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            bool isAlive;
+            char grade;
+            decimal weight;
+            double bankAmount;
+
+            age = 24;
+            name = "Tyler";
+            isAlive = true;
+            grade = 'A';
+            weight = 222.22M;
+            bankAmount = 2000;
+
+            Console.WriteLine($"I am {age} years old. My name is {name}. The grades I get are {grade}. I weigh {weight}lbs. I have {bankAmount} dollars in my bank accounr. Is he alive {isAlive}");
+        }
+    }
+}
